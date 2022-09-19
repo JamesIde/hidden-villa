314f0c11550d3b0512ef357d563dca286e1c19c9
James Ide <james.ide775@gmail.com>
2022-09-19 12:54:55 +0930

    Update index.ts

--- server/index/index.ts
+++ server/index/index.ts
@@ -1,34 +1,34 @@
-import express, { Request, Response } from "express"
-import cookieParser from "cookie-parser"
-import cors from "cors"
-import "colors"
-import "dotenv/config"
+import express, { Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import "colors";
+import "dotenv/config";
 // Set up app and port
-const app = express()
+const app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 // Allow json and urlEncoded data
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Cookie parser
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:4200",
+    origin: "https://hidden-villa-1.web.app/",
     credentials: true,
   })
-)
+);
 // Welcome route
 app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to the hotel booking API")
-})
+  res.send("Welcome to the hotel booking API");
+});
 
-app.use("/api/hotels", require("./modules/hotels/hotelRoutes"))
-app.use("/api/auth", require("./modules/auth/authRoutes"))
-app.use("/api/payment", require("./modules/booking/bookingRoutes"))
+app.use("/api/hotels", require("./modules/hotels/hotelRoutes"));
+app.use("/api/auth", require("./modules/auth/authRoutes"));
+app.use("/api/payment", require("./modules/booking/bookingRoutes"));
 
 app.listen(process.env.PORT, () => {
-  console.log(`🚀 Server started on port ${port}`.cyan.underline)
-})
+  console.log(`🚀 Server started on port ${port}`.cyan.underline);
+});
