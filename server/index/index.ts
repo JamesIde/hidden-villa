97b2b38a79a91f6db693fe890520c37134328e0f
James Ide <james.ide775@gmail.com>
2022-09-19 14:01:28 +0930

    Fix server domain url

--- server/index/index.ts
+++ server/index/index.ts
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://hidden-villa-1.web.app/",
+    origin: "https://hidden-villa-1.web.app",
     credentials: true,
   })
 );
