175df24163a7dfecd7579c656b9f8b0e63400f78
James Ide <james.ide775@gmail.com>
2022-09-19 15:04:03 +0930

    More deployment fixes =(

--- server/index/index.ts
+++ server/index/index.ts
@@ -20,6 +20,7 @@ app.use(
     // origin: "http://localhost:4200",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
+    exposedHeaders: ["set-cookie"],
   })
 );
 // Welcome route
